540d65876c257bb73da7618235ad897483c791db
Fadhli <marvelouslyfadhli@gmail.com>
2023-08-26 04:26:24 +0800

    fix: login page template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,22 @@
-<script></script>
+<script>
+    export default{
+        data(){
+            return {
+                username: "username",
+                password: "password"
+            };
+        },
+        methods:{
+            onSubmit(e){
+                e.preventDefault();
+                console.log(this.username);
+                console.log(this.password);
+                console.log("login clicked");
+            }
+        }
+
+    }
+</script>
 
 <template>
 
@@ -6,31 +24,22 @@
         <div class="grid grid-cols-2 gap-4">
             <div class="font-bold text-3xl">
                 <h1>Learning Made Easy</h1>
-                <h1>with <a class="text-5xl text-[#F58B2E]">SensorySpark</a></h1>
+                <h1>with <a class="text-5xl text-orange">SensorySpark</a></h1>
                 <img src="../assets/mascot.png" height="300" width="300" class="ms-14 mt-5"/>
             </div>
             <div>
                 <div>
-                    <form>
+                    <form @submit="onSubmit">
                         <div class="mt-5">
-                            <label for="username" class="text-2xl font-bold ms-3">Username</label><br />
+                            <label for="username" class="text-2xl font-bold ms-3">Username/Email</label><br />
                             <input
                                 type="text"
                                 id="username"
                                 name="username"
-                                class="border-dashed border-2 border-[#F58B2E] rounded-full w-full p-2"
+                                v-model="username"
+                                class="border-dashed border-2 border-orange rounded-full w-full p-2 bg-orange-25 focus:outline-none"
                             />
                         </div>
-
-                        <div class="mt-5">
-                            <label for="email" class="text-2xl font-bold ms-3">Email</label><br />
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                class="border-dashed border-2 border-[#F58B2E] rounded-full w-full p-2"
-                            />
-                            </div>
 
                         <div class="mt-5">
                             <label for="password" class="text-2xl font-bold ms-3">Password</label><br />
@@ -38,7 +47,8 @@
                                 type="password"
                                 id="password"
                                 name="password"
-                                class="border-dashed border-2 border-[#F58B2E] rounded-full w-full p-2"
+                                v-model="password"
+                                class="border-dashed border-2 border-orange rounded-full w-full p-2 bg-orange-25 focus:outline-none"
                             />
                         </div>
 
@@ -47,13 +57,13 @@
                                 <input
                                     type="submit"
                                     value="Login"
-                                    class="bg-[#F58B2E] rounded-full px-5 py-3 w-full cursor-pointer"
+                                    class="bg-orange rounded-full px-5 py-3 w-full text-xl cursor-pointer font-bold"
                                 />
                             </div>
                             
                             <div class="mx-auto">
                             <div class="py-3">
-                                <a href="/" class="underline text-[#F58B2E] w-full">Sign up</a>
+                                <a href="/" class="underline text-orange w-full text-xl font-bold">Sign up</a>
                             </div>
                             </div>
                         </div>                    
