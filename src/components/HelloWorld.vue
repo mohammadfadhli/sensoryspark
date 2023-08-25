770045eeae411933347b502ada1abd1e858953eb
xuanli286 <xuanli743@gmail.com>
2023-08-26 01:56:56 +0800

    Add tailwind config file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="text-green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
@@ -19,26 +19,4 @@ defineProps({
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false,
+  theme: {
+    colors: {
+      black: "#27272D",
+      orange: "#F58B2E",
+      yellow: "#FFF584",
+      green: "#D5CB6F",
+    },
+    fontFamily: {
+      mono: ["Consolas"],
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    }
+  },
+  variants: {
+    extend: {
+    },
+  },
+};
