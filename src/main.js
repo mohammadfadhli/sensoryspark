e07bff7785673487cf630dce393f294bc8f8dae8
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-08-26 02:13:22 +0800

    add: login page route

--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Login from "./views/Login.vue";
+
+const routes = [
+    {path: "/login", component: Login}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(router)
 
 app.mount('#app')
