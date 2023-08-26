3254c4e4c2697532e53184070883b9716e01d9f4
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-08-26 14:59:23 +0800

    fix: chat page

--- src/views/TutorGPT.vue
+++ src/views/TutorGPT.vue
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-<script setup>
-
-</script>
-
-<template>
-
-    <div class="container mx-auto w-full h-screen">
-        <iframe src="http://localhost:8501/" class="w-full h-full"></iframe>
-    </div>
-
-</template>
-=======
 <script>
 </script>
 
 <template>
     
 </template>
->>>>>>> 06d702ecd9b8c0ee20ced80e80d50d1711f8394d
