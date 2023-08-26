3459caa1b3f412469bf50ecb4691eec895ce86d5
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-08-26 22:02:00 +0800

    fix: merge conflicts

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -16,7 +16,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const auth = getAuth(app);
-export {auth}
+
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);  
+
+const auth = getAuth(app);
+export {auth}
