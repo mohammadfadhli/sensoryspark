00fc3f99fea4b0f75cdf2ab195d0b9bb47e336d9
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-08-26 18:18:24 +0800

    added tutorgpt endpoint

--- src/views/TutorGPT.vue
+++ src/views/TutorGPT.vue
@@ -4,7 +4,8 @@
 <template>
 
     <div class="container mx-auto w-full h-screen">
-        <iframe src="http://localhost:8501/" class="w-full h-full"></iframe>
+        <!-- <iframe src="http://localhost:8501/" class="w-full h-full"></iframe> -->
+        <iframe src="https://bogus-grape-ln6i.mohammadfadhli.zeet.app" class="w-full h-full"></iframe>
     </div>
     
 </template>
