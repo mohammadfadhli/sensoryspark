a4b2181f9960c8187fff72081cc8b2694a19e2ca
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-08-27 16:05:39 +0800

    feat: added title to tutorgpt page

--- src/views/TutorGPT.vue
+++ src/views/TutorGPT.vue
@@ -4,6 +4,9 @@
 <template>
     <div class="container mx-auto w-full h-screen">
         <!-- <iframe src="http://localhost:8501/" class="w-full h-full"></iframe> -->
+
+        <h1 class="font-bold text-5xl text-orange text-center my-5">TutorGPT</h1>
+
         <iframe
             src="https://bogus-grape-ln6i.mohammadfadhli.zeet.app"
             class="w-full h-full"
