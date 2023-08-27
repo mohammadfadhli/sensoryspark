63f9b1e66cccc7276f96ab13fbba223aee87b09f
xuanli286 <xuanli743@gmail.com>
2023-08-27 14:42:09 +0800

    Add responsiveness to card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -59,16 +59,16 @@ const shapes = {
 </script>
 
 <template>
-    <div class="card relative sm:w-10 sm:h-10 md:w-24 md:h-24 lg:w-32 lg:h-32 relative text-center preserve-3d rotate-y-180 transition ease-in duration-500 cursor-pointer hover:bg-yellow flex flex-wrap justify-center content-center" 
+    <div class="card relative md:w-24 md:h-24 w-16 h-16 relative text-center preserve-3d rotate-y-180 transition ease-in duration-500 cursor-pointer hover:bg-yellow flex flex-wrap justify-center content-center" 
         :class="visible ? 'bg-yellow border border-green w-full h-full rotate-y-180 absolute rounded-lg' : 'bg-orange w-full h-full rounded-lg'"
         @click="selectCard"
     >
-        <div v-if="visible">
-            <p>{{ value }}</p>
-            <img class="mx-auto md:w-12 md:h-12 lg:w-24 lg:h-24" :src="`${shapes[value]}`"/>
+        <div v-if="visible" class="py-auto">
+            <p class="text-xs md:text-base">{{ value }}</p>
+            <img class="mx-auto md:w-12 md:h-12 w-8 h-8" :src="`${shapes[value]}`"/>
         </div>
         <div v-else>
-            <img class="mx-auto md:w-16 md:h-16 lg:w-28 lg:h-28" src="@/assets/backmonster.png">
+            <img class="mx-auto md:w-16 md:h-16 w-10 h-10" src="@/assets/backmonster.png">
         </div>
     </div>
 </template>
