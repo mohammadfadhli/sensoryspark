0358f497b49d8f3f491cc3f8690d910f0e6106f7
EnqiChan <enqi.chan.2021@scis.smu.edu.sg>
2023-08-27 12:34:58 +0800

    broken ui

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,3 +1,4 @@
+<!--ignore test.vue and PostCard.vue-->
 <script>
 import {app, db} from '../../firebaseConfig.js';
 import { collection, addDoc } from "firebase/firestore"; 
@@ -34,6 +35,15 @@ export default{
         }   
     }
 }
+
+
+
+import { ref } from 'vue';
+import { usePosts } from '../stores/counter.js'; // Replace with your actual store import
+import PostCard from './PostCard.vue'; // Import the PostCard component
+
+const posts = usePosts(); // Assuming you have a store module for posts
+
 </script>
 
 <template>
@@ -58,42 +68,16 @@ export default{
             </div>
             <button type="submit" class="bg-orange rounded-full px-5 py-3 font-bold my-2">Submit</button>
         </form>
-    </div>
 
-
-<!--card-->
-    <div class="container max-w-[1024px] mx-auto p-4">
-        <div class="bg-fff584 border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div class="mb-8">
-                <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?
-                </div>
-                <div class="flex items-center">
-                    <img class="w-10 h-10 rounded-full mr-4" src="../assets/mascot.png" alt="Name of user">
-                    <div class="text-sm">
-                        <p class="text-gray-900 leading-none">Name of user</p>
-                        <p class="text-gray-600">Post date</p>
-                    </div>
-                </div>
-                <div class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                </div>
-            </div>
+        <div v-for="post in posts" :key="post.id">
+            <PostCard :post="post" />
         </div>
-    </div>
 
+    </div>
 </template>
 
 
-<style scoped>
-.bg-fff584 {
-    background-color: #fff584;
-}
-
-.border-d5cb6f {
-    border-color: #d5cb6f;
-}
-
-.rounded-card {
-    border-radius: 10px;
-}
+<style>
+/* Add custom styles here if needed */
 </style>
+
